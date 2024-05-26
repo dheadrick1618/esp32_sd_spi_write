83e0baeaead5e5495d5b88fbb4ee73c752b8221f
Devin <headrick@ualberta.ca>
2024-05-26 11:21:42 -0600

    Writes to a new directory each time its power cycled. Writes to a file a max number of times, then writes to a new file.

--- main/esp32_sd_spi.c
+++ main/esp32_sd_spi.c
@@ -1,8 +1,14 @@
 /*
 Written by Devin Headrick
 
-To change the pinout for a different board, un-comment the desired define in config.h
+This program writes to a new directory every powercycle.
+
+Note:
+-To change the pinout for a different board, un-comment the desired define in config.h
+
+TODO - For each new file to write, check if there is space on the SD card for the new file (with margin)
 */
+
 #include <stdio.h>
 #include <string.h>
 #include "esp_err.h"
@@ -10,33 +16,31 @@ To change the pinout for a different board, un-comment the desired define in con
 #include <sys/stat.h>
 #include "esp_vfs_fat.h"
 #include "sdmmc_cmd.h"
-
 #include "esp_timer.h"
 #include <rom/ets_sys.h> //For ets_delay_us()
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
-
+#include <math.h>
+#include <dirent.h>
 #include "config.h"
 
 #define MOUNT_POINT "/sdcard"
-const char *file_path = MOUNT_POINT "/new_data.txt";
-
-static const char *TAG = "esp32_sd_spi";
-
 #define DATA_CHUNK_SIZE 32768 // Size of a 'chunk' of data (bytes) for each write operation
-
 #define QUEUE_LENGTH 1
 #define QUEUE_ITEM_SIZE sizeof(data_chunk_t)
 #define MEASURE_INTERVAL_MS 5000 // Measure interval in milliseconds
 #define DATA_GEN_DELAY_US 1
-
-#define FILE_COUNT_MAX 10
-#define NUM_WRITES_PER_FILE_MAX 1000
-
+#define NUM_WRITES_PER_FILE_MAX 30
+// Recall 4 GB is not 4^9 Bytes, rather 4 *1024 * 1024 * 1024 bytes
+#define SD_CARD_SIZE_BYTES 4194304 // Assume 4 GB MAX (given we are using Single Level Cell [SLC] type mem for flight hardyness)
 static QueueHandle_t data_queue;
 static uint32_t total_bytes_written = 0;
+
+static const char *TAG = "esp32_sd_spi";
+
+char new_dir_name[32];
+char file_path[128];
 
 typedef struct
 {
@@ -53,7 +57,6 @@ static void data_generator_task(void *param)
     {
         chunk.data[i] = 'A' + (i % 26); // Cycle between ASCII capital letters
     }
-
     while (1)
     {
         // Place data chunk onto the queue
@@ -72,30 +75,64 @@ static void data_generator_task(void *param)
 static int write_data_to_file(const char *path, char *data)
 {
     int written_bytes = 0;
-    // Open the file
-    FILE *file = fopen(path, "a");
+    FILE *file = fopen(path, "w");
     if (file == NULL)
     {
         ESP_LOGE(TAG, "Failed to open file for appending");
-        return ESP_FAIL;
+        return -1;
     }
     // Set to NULL buffer (dynamically determined buffer size), Full Buffering, with larger stream buffer size
     setvbuf(file, NULL, _IOFBF, 32 * 1024);
 
-    // Write the data
-    written_bytes = fwrite(data, sizeof(char), DATA_CHUNK_SIZE, file);
+    written_bytes = fwrite(data, sizeof(char), DATA_CHUNK_SIZE, file); // Perform file write
     if (written_bytes < 0)
     {
         ESP_LOGE(TAG, "Failed to write data to the file");
+        return -1;
     }
-    // TODO - try writing data using fwrite as we do not need formatting provided by fprintf with the raw byte data
-
     fclose(file);
-
     return written_bytes;
 }
 
-// Task to write data from the queue to the SD card
+/// @brief Get the number associated with the previously written directory (largest appended num)
+/// NOTE the FAT FS seems to write dir names as uppercase, so we want to write dirs with captial letters
+/// @return the appended file num of the previous directory used for writing
+int get_previous_written_dir_num()
+{
+    DIR *dir = opendir(MOUNT_POINT);
+    if (!dir)
+    {
+        ESP_LOGE(TAG, "Failed to open directory");
+        return -1;
+    }
+
+    int max_index = -1;
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL)
+    {
+        ESP_LOGI(TAG, "Found entry: %s", entry->d_name);
+        if (entry->d_type == DT_DIR)
+        {
+            int index;
+            if (sscanf(entry->d_name, "DIR%d", &index) == 1)
+            {
+                if (index > max_index)
+                {
+                    max_index = index;
+                }
+            }
+            else
+            {
+                ESP_LOGI(TAG, "Directory name does not match format: %s", entry->d_name);
+            }
+        }
+    }
+    closedir(dir);
+
+    return max_index;
+}
+
+/// @brief Responsible for writing data from a queue (from the receiver core) onto an SD card as fast as possible
 static void sd_card_writer_task()
 {
     data_chunk_t chunk;
@@ -105,43 +142,32 @@ static void sd_card_writer_task()
     int file_count = 1;
     int num_writes_per_file = 0;
 
-    const char str[80] = "/sdcard/file_0.txt";
-
     while (1)
     {
         // Wait for data to arrive on the queue
         if (xQueueReceive(data_queue, &chunk, portMAX_DELAY) == pdPASS)
         {
-            // total_bytes_written += (uint32_t)write_data_to_file(file_path, &chunk.data);
-
-            if (file_count < FILE_COUNT_MAX)
+            if (num_writes_per_file < NUM_WRITES_PER_FILE_MAX)
             {
-                if (num_writes_per_file < NUM_WRITES_PER_FILE_MAX)
-                {
-                    total_bytes_written += (uint32_t)write_data_to_file(str, &chunk.data);
-                    num_writes_per_file += 1;
-                }
-                else
-                {
-                    // Incremement the file name by 1
-                    sprintf(str, "/sdcard/file_%d.txt", file_count);
-                    file_count++;
-                    num_writes_per_file = 0;
-                }
+                total_bytes_written += (uint32_t)write_data_to_file(file_path, &chunk.data);
+                num_writes_per_file += 1;
             }
             else
             {
-                ESP_LOGI(TAG, "Max num files written\n");
-                vTaskDelay(100000/portTICK_PERIOD_MS);
+                // Incremement the file name by 1
+                sprintf(file_path, "%s/FILE_%d.txt", new_dir_name, file_count);
+                file_count++;
+                num_writes_per_file = 0;
+                ESP_LOGI(TAG, "FILE_%d has been written max times. Now writing to FILE_%d", file_count - 1, file_count);
             }
         }
+        //-------------------------------------------------------------------
         elapsed_time_us = esp_timer_get_time() - start_time_us;
-
         if (elapsed_time_us >= MEASURE_INTERVAL_MS * 1000)
         {
             float write_speed_Bps = (total_bytes_written) / (elapsed_time_us / 1000000.0); // in bits per second
             ESP_LOGI(TAG, "Write Speed: %.5f Bytes per second ", write_speed_Bps);
-            // Reset counters
+            // Reset timer counters
             total_bytes_written = 0;
             start_time_us = esp_timer_get_time();
         }
@@ -156,7 +182,7 @@ void app_main(void)
 
     esp_vfs_fat_mount_config_t mount_config = {
         .format_if_mount_failed = false,
-        .max_files = FILE_COUNT_MAX,
+        .max_files = 4,
         .allocation_unit_size = 64 * 1024};
 
     sdmmc_card_t *card;
@@ -204,11 +230,28 @@ void app_main(void)
     }
 
     ESP_LOGI(TAG, "Filesystem mounted");
+    // Now use POSIX and C standard library functions to work with files thanks to VFS FAT FS mounting.
 
     // Card has been initialized, print its properties
     sdmmc_card_print_info(stdout, card);
 
-    // Now use POSIX and C standard library functions to work with files thanks to VFS FAT FS mounting.
+    int prev_dir_num = get_previous_written_dir_num();
+    ESP_LOGI(TAG, "Prev written dir num: %d \n", prev_dir_num);
+    int current_dir_num = prev_dir_num + 1;
+
+    // Create a new directory with the next index
+    snprintf(new_dir_name, sizeof(new_dir_name), "%s/DIR%d", MOUNT_POINT, current_dir_num);
+    if (mkdir(new_dir_name, 0775) != 0)
+    {
+        ESP_LOGE(TAG, "Failed to create directory %s", new_dir_name);
+    }
+    else
+    {
+        ESP_LOGI(TAG, "Directory %s created", new_dir_name);
+    }
+    // Define the file path to the first file to be written to the new directory
+    sprintf(file_path, "%s/FILE_0.txt", new_dir_name);
+    ESP_LOGI(TAG, "Writing to this path now: %s \n", file_path);
 
     // Create a FreeRTOS queue for byte data to be shared between tasks on different cores
     data_queue = xQueueCreate(QUEUE_LENGTH, QUEUE_ITEM_SIZE);
@@ -221,6 +264,6 @@ void app_main(void)
     xTaskCreatePinnedToCore(sd_card_writer_task, "sd_card_writer_task", 77777, NULL, 7, NULL, 1); // Run on core 0
     xTaskCreatePinnedToCore(data_generator_task, "data_generator_task", 77777, NULL, 5, NULL, 0); // Run on core 1
 
-    //Dont need to worry about freeing memory or unmounting cards as this program is 
-    // expected to run from power on until power off 
+    // Dont need to worry about freeing memory or unmounting cards as this program is
+    // expected to run from power on until power off.
 }
