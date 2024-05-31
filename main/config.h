1a600346a9b6c2d2027249ed79f4b73023bfc3fe
Devin <headrick@ualberta.ca>
2024-05-30 18:35:12 -0600

    Started spi read task (for getting data from fpga). Not tested and needs to replace data gen task still

--- main/config.h
+++ main/config.h
@@ -4,15 +4,22 @@
 // Define the version macro to choose the pinout configuration
 // Uncomment one of the following lines to select the device version
 #define ESP32_WROOM
-// #define OTHER
+// #define NODEMCU
 
 #ifdef ESP32_WROOM
-    #define PIN_NUM_MISO 2  // D0
-    #define PIN_NUM_MOSI 15 // D3
-    #define PIN_NUM_CLK 14  // SCK
-    #define PIN_NUM_CS 13   // CMD
+    //Currently using HSPI line for SD card interfacing
+    #define PIN_SD_MISO 2  // D0
+    #define PIN_SD_MOSI 15 // D3
+    #define PIN_SD_CLK 14  // SCK
+    #define PIN_SD_CS 13   // CMD
 
-#elif OTHER
+    // Using VSPI line for data source (FPGA) interfacing 
+    #define PIN_FPGA_MOSI 1
+    #define PIN_FPGA_MISO 2
+    #define PIN_FPGA_CLK  3
+    #define PIN_FPGA_CS   4
+
+#elif NODEMCU
     #define PIN_NUM_MISO 19  // D0
     #define PIN_NUM_MOSI 23 // D3
     #define PIN_NUM_CLK 18  // SCK
